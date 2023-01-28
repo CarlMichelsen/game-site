9039f9b133720924d3675ae0bea43b9bfad4d976
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2023-01-28 19:39:08 +0100

    Removed about page.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,18 +3,12 @@ import GitHub from '@auth/core/providers/github';
 import { SvelteKitAuth, type SvelteKitAuthConfig } from '@auth/sveltekit';
 import { environment } from '$lib/environment';
 
-//console.log('secret', environment.githubAuthClientId, environment.githubAuthSecret);
-
 const authConfigGithub = GitHub({
 	clientId: environment.githubAuthClientId,
 	clientSecret: environment.githubAuthSecret
 });
 const config: SvelteKitAuthConfig = {
-	prefix: '/auth',
 	providers: [authConfigGithub] as Provider[]
 };
 
 export const handle = SvelteKitAuth(config);
-
-// disgusting
-// https://www.reddit.com/r/sveltejs/comments/1030h1s/authjs_sveltekit/
