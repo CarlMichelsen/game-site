2cb05e81628687f776b17aa37cce123829d9caff
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2023-01-28 18:08:39 +0100

    Testing env vars.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,10 @@
 import type { Provider } from '@auth/core/providers';
 import GitHub from '@auth/core/providers/github';
 import { SvelteKitAuth, type SvelteKitAuthConfig } from '@auth/sveltekit';
+import dotenv from 'dotenv';
+dotenv.config();
 import { GITHUB_AUTH_CLIENT_ID, GITHUB_AUTH_SECRET } from '$env/static/private';
 
-console.log('Very secret', GITHUB_AUTH_CLIENT_ID, GITHUB_AUTH_SECRET);
 const authConfigGithub = GitHub({
 	clientId: GITHUB_AUTH_CLIENT_ID,
 	clientSecret: GITHUB_AUTH_SECRET
