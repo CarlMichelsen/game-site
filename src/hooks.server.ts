54cade5978de81bbeefd0d7666f4b87342f0a96a
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2023-01-30 21:10:09 +0100

    .

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,10 +7,11 @@ const authConfigGithub = GitHub({
 	clientId: environment.githubAuthClientId,
 	clientSecret: environment.githubAuthSecret
 });
-const providers = [authConfigGithub] as Provider[];
+
 const config: SvelteKitAuthConfig = {
 	trustHost: environment.development,
-	providers: environment.development ? [] : providers
+	secret: environment.development ? 'very secret' : undefined,
+	providers: [authConfigGithub] as Provider[]
 };
 
 export const handle = SvelteKitAuth(config);
