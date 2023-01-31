76a31a746cad88e3450d09492176c096ebe7106d
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2023-01-31 18:43:27 +0100

    Added attempt to redirect on login.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Provider } from '@auth/core/providers';
 import GitHub from '@auth/core/providers/github';
 import { SvelteKitAuth, type SvelteKitAuthConfig } from '@auth/sveltekit';
 import { environment } from '$lib/environment';
+import { redirect } from '@sveltejs/kit';
 
 const authConfigGithub = GitHub({
 	clientId: environment.githubAuthClientId,
@@ -11,6 +12,11 @@ const authConfigGithub = GitHub({
 const config: SvelteKitAuthConfig = {
 	trustHost: environment.development,
 	secret: environment.authSecret,
+	events: {
+		async signIn() {
+			redirect(302, '/home');
+		}
+	},
 	providers: [authConfigGithub] as Provider[]
 };
 
